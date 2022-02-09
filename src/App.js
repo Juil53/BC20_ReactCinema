9f19150c9b93cb561ce4841d1d7582cc31c1235d
THANH VUONG <nguyenthanhvuong537@gmail.com>
2022-02-09 14:00:17 +0700

    repush

--- src/App.js
+++ src/App.js
@@ -6,24 +6,21 @@ import Footer from "./container/HomeTemplate/_component/Footer";
 import { renderRouteHome } from "./routes";
 import Layout from "./container/HomeTemplate/_component/Layout/Layout";
 import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
-import CustomTheme from "./Utils/CustomTheme";
+
 function App() {
   return (
-    <ThemeProvider theme={CustomTheme}>
-      <BrowserRouter>
-        <CssBaseline />
-        <Layout>
-          <NavBar />
-          <Switch>
-            {renderRouteHome()}
-            {/* Trang PageNotFound */}
-            <Route path="" component={PageNotFound} />
-          </Switch>
-          <Footer />
-        </Layout>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <CssBaseline />
+      <Layout>
+        <NavBar />
+        <Switch>
+          {renderRouteHome()}
+          {/* Trang PageNotFound */}
+          <Route path="" component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </Layout>
+    </BrowserRouter>
   );
 }
 
